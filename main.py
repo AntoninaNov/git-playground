8024220c307fd14d7b9900828bd7a22319620209
Antonina Novak <anovak@gmail.com>
2022-12-02 01:44:09 +0200

    Updated guesses checking. Added a warning of a doubled word.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import words_fetcher
 import random
+from itertools import permutations
 
 
 def congratulate_user():
@@ -25,19 +26,31 @@ words = words_fetcher.fetch_words(min_letters=9, max_letters=9)
 full_list = words_fetcher.fetch_words(min_letters=3, max_letters=9)
 word = words[random.randrange(0, len(words))]
 
+word_letters = set(word)
+appropriate_words = set()
+guesses = set()
+
+for n in range(3, len(word)):
+    for y in list(permutations(word_letters, n)):
+        permuted_word = "".join(y)
+        if permuted_word in full_list:
+            appropriate_words.add(permuted_word)
+
 print(f"Can you make up {WORDS_TO_WIN} words from letters in word provided by me?")
 print(f"Your word is '{word}'")
 
 
 while not is_game_over():
     guess = input("Your next take: ")
-    if guess in full_list:
+    if guess in appropriate_words - guesses:
         guessed += 1
-        guesses.append(guess)
+        guesses.add(guess)
         if guessed == WORDS_TO_WIN:
             congratulate_user()
             exit()
         print(f"That's right! {WORDS_TO_WIN - guessed} to go")
+    elif guess in guesses:
+        print("You have already used that word. Choose another.")
     else:
         errors += 1
         print(f"Oops :( No such word, you have {ERRORS_TO_LOSE - errors} lives more")
